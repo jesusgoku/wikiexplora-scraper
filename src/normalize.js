7f132cb7785de68416c66b81c9830db074253f92
Jesus Urrutia <jesus.urrutia@gmail.com>
2018-04-27 19:08:32 -0300

    Added info for normalize

---
+++ src/normalize.js
@@ -0,0 +1,161 @@
+const options = {
+  'Actividad': [
+    'Trekking',
+    'Mountain Bike',
+    'Randonnée',
+    'Montaña'
+  ],
+
+  'Altitud (metros)': 'msmn',
+  'Primer ascenso: (Opcional)': '',
+
+  'País': [
+    'Argentina',
+    'Bolivia',
+    'Brasil',
+    'Chile',
+    'Colombia',
+    'Ecuador',
+    'Perú',
+    'Chile (english)'
+  ],
+
+  'Ciudad más cercana': {
+    'Argentina': [],
+    'Bolivia': [],
+    'Brasil': [],
+    'Chile': [],
+    'Colombia': [],
+    'Ecuador': [],
+    'Perú': [],
+  },
+
+  'Sector': [
+    'Valle Aconcagua',
+    'Valle Mapocho',
+    'Valle Maipo',
+    'Cordillera Costa',
+  ],
+
+  'Belleza Escénica': [
+    'Impresionante',
+    'Atractiva',
+    'Mediana',
+    'Discreta',
+  ],
+
+  'Atractivos': [
+    'Vistas panorámicas',
+    'Glaciar',
+    'Bosque',
+    'Fuente Terma',
+    'Flora atractiva',
+    'Fauna atractiva',
+    'Géyser',
+    'Lago',
+    'Laguna',
+    'Río',
+    'Mar',
+    'Fiordos',
+    'Isla',
+    'Formación Geológica',
+    'Cascada',
+    'Patrimonio Cultural',
+    'Parque Nacional',
+    'Parque Privado',
+    'Páramo',
+  ],
+
+  'Temperatura Terma': [
+    'Muy calientes',
+    'Óptimas',
+    'Tibias',
+    'Frías',
+    'De todo tipo',
+  ],
+
+  'Duración': [
+    '3 horas o menos',
+    '1/2 día',
+    '3/4 día',
+    '1 día',
+    '1 día y medio',
+    '2 días',
+    '3 días',
+    '4 días',
+    '5 días',
+    '3 - 5 días',
+    '6 - 7 días',
+    '8 - 10 días',
+    '11 - 14 días',
+    '15 - 20 días',
+    '20 - 25 días',
+    '26 - 35 días',
+    '36 - 60 días',
+    '61 - 89 días',
+    'más de 90 días',
+  ],
+
+  'Requiere de habilidades': [
+    'Sí requiere',
+    'No requiere',
+  ],
+
+  'Habilidades': [
+    'Marcha en glaciares',
+    'Cruce de ríos caudalosos',
+    'Orientación con GPS o Mapas',
+    'Rapel con cuerdas',
+    'Pasos en roca',
+    'Asegurar con cuerdas',
+  ],
+
+  'Duración (en MTB)': [
+    '3 horas o menos',
+    '1/2 día',
+    '3/4 día',
+    '1 día',
+    '1 día y medio',
+    '2 días',
+    '3 días',
+    '4 días',
+    '5 días',
+    '3 - 5 días',
+    '6 - 7 días',
+    '8 - 10 días',
+    '11 - 14 días',
+    '15 - 20 días',
+    '20 - 25 días',
+    '26 - 35 días',
+    '36 - 60 días',
+    '61 - 89 días',
+    'más de 90 días',
+  ],
+
+  'Dificultad Técnica MTB': ['Muy alta', 'Alta', 'Media', 'Baja'],
+
+  'Requiere de habilidades de Mountain Bike': ['Sí requiere', 'No requiere'],
+
+  'Habilidades de Mountain Bike': ['Descenso pronunciado', 'Ascensos Pronunciados'],
+
+  'Claridad Sendero': ['Siempre Claro', 'Tramos sin sendero', 'Gran parte sin sendero', 'Inexistente'],
+
+  'Nivel de Señalización': ['Adecuado', 'Suficiente', 'Insuficiente', 'Inexistente'],
+
+  'Infraestructura': ['Hotel(es)', 'Cabaña(s)', 'Hospedaje(s)', 'Refugio(s)', 'Camping', 'Inexistente'],
+
+  'Tipo de recorrido': ['Ida y Retorno por la misma ruta', 'Circuito', 'Cruce'],
+
+  'Distancia': 'metros',
+  'Metros de ascenso': 'metros',  // Usar perfil de elevación en google maps
+  'Metros de descenso': 'metros',
+  'Comentarios distancia y desnivel': '',
+  'Altitud media': '',
+  'Recomendaciones a incluir': ['Altura', 'Radiación Solar'],
+  'Primer Autor': '',
+  'Imágen Principal': '',
+  'Comentarios a la imágen': '',
+  'KMZ de la ruta': '',
+  'Tipo de Mapa': ['SATELLITE', 'TERRAIN', 'HYBRID'],
+  'Comentarios al mapa': '',
+};
